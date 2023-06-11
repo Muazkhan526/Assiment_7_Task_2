40379bdf14fb1a7185e1d2ab9094b53d4edba772
Muhammad Muaz <110101623+Muazkhan526@users.noreply.github.com>
2023-06-12 01:11:23 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import React, {Component} from "react";
+import Buttons from '../src/Calculater/buttons';
+import Result from '../src/Calculater/Result';
+import "../src/Calculater/calc.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: "0",
+    }
+    this.handleclicks = this.handleclicks.bind(this);
+  }
+
+  handleclicks = (value) => {
+    switch (value) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '.':
+      case '+':
+      case '-':
+      case '/':
+      case '*':
+        if (this.state.result === "0") {
+          this.setState({
+            result: value
+          });
+
+        } else {
+          this.setState({
+            result: this.state.result + value
+          });
+        }
+        break;
+      case '=':
+        console.log(this.state.result);
+        this.setState({
+          result: eval(this.state.result)
+        })
+        break;
+      case 'c':
+        this.setState({
+          result: ''
+        })
+        break;
+      case '+/-':
+        this.setState({
+          result: parseInt(this.state.result, 10) * -1
+        });
+        break;
+      case '%':
+        this.setState({
+          result: parseFloat(this.state.result, 10) / 100
+        });
+        break;
+      default:
+        break;
+    }
+
+  }
+
+  render() {
+    return ( <
+      div className = "mycalc" >
+      <
+      Result result = {
+        this.state.result
+      }
+      /> <
+      Buttons Buttonclicked = {
+        this.handleclicks
+      }
+      /> 
+      </div>
+    );
+  }
+}
+
+export default App;
